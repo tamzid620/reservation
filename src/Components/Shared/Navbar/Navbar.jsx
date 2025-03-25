4b8a1c9a228d178571388b4ef340a0865269e99c
Tamzid_Ull_Monir <121627944+tamzid620@users.noreply.github.com>
2025-03-25 15:35:32 +0600

    Update Navbar.jsx

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 
+import mainIcon from '../../../assets/Icons/SkyScape-Logo.png'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav style={{fontFamily : "Barlow, sans-serif "}} >
+            <div className="lg:max-w-6xl md:max-w-3xl sm: w-full">
+                {/* icon section  */}
+                <div>
+                    <div className="relative flex items-center">
+                        <h1 className="relative text-2xl font-extrabold uppercase"
+                        >SkyScape</h1>
+                        <img src={mainIcon} alt="" className="w-[60px]" />
+                        <p className="absolute top-8">explore the world</p>
+                    </div>
+                </div>
+                {/* ul li section  */}
+                <div></div>
+            </div>
+        </nav>
     );
 };
 
