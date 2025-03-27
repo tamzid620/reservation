d0b4d11eabd6468dd4e54e6312faf2571f8fa430
tamzid620 <tamzid620@gmail.com>
2025-03-27 12:21:49 +0600

    navbar added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
- Demo website : https://webredox.net/demo/wp/travol/catalog/#
+ Demo website : https://webredox.net/demo/wp/travol/catalog/# , https://gozayaan.com/ 
  
 bannertext {
 color: transparent;
@@ -9,4 +9,9 @@ color -1: #3674B5 ;
 color -2: #578FCA ;
 color -3: #A1E3F9 ;
 color -4: #D1F8EF ;
+
+<!-- style list  -->
+
+style={{ fontFamily: "Barlow, sans-serif " }}
+
 
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,24 +1,110 @@
+import { Link } from "react-router-dom";
+import mainIcon from "../../../assets/Icons/SkyScape-Logo.png";
+import menuIcon from "../../../assets/Icons/menu.png";
+import { useState } from "react";
 
-import mainIcon from '../../../assets/Icons/SkyScape-Logo.png'
+const userNavUl = [
+  {
+    path: "/",
+    title: "Home ",
+  },
+  {
+    path: "/",
+    title: " About",
+  },
+  {
+    path: "/",
+    title: "Blog ",
+  },
+  {
+    path: "/",
+    title: "Gallery ",
+  },
+  {
+    path: "/",
+    title: " Contact",
+  },
+];
 
 const Navbar = () => {
-    return (
-        <nav style={{fontFamily : "Barlow, sans-serif "}} >
-            <div className="lg:max-w-6xl md:max-w-3xl sm: w-full">
-                {/* icon section  */}
-                <div>
-                    <div className="relative flex items-center">
-                        <h1 className="relative text-2xl font-extrabold uppercase"
-                        >SkyScape</h1>
-                        <img src={mainIcon} alt="" className="w-[60px]" />
-                        <p className="absolute top-8">explore the world</p>
-                    </div>
-                </div>
-                {/* ul li section  */}
-                <div></div>
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toogleNavbar = () => {
+    setNavOpen(!navOpen);
+  };
+
+  return (
+    <nav style={{ fontFamily: "Barlow, sans-serif " }}>
+      <div className="lg:max-w-6xl md:max-w-3xl sm: w-full  mx-auto">
+        {/*/ ******************************* navlist for large & medium device  ******************************* */}
+        <div className="md:flex justify-between items-center sm: hidden py-3 px-2">
+          {/* ------------------ icon section ------------------ */}
+          <div>
+            <div className="relative flex items-center">
+              <h1 className="relative text-2xl font-extrabold uppercase text-white">
+                SkyScape
+              </h1>
+              <img src={mainIcon} alt="" className="w-[60px]" />
+              <p className="absolute top-9 left-14 text-[10px] font-semibold text-white">
+                explore the world
+              </p>
             </div>
-        </nav>
-    );
+          </div>
+          {/* ------------------ ul li section ------------------ */}
+          <div>
+            <ul className="flex gap-10 ">
+              {userNavUl.map(({ path, title }, index) => (
+                <Link key={index} to={path}>
+                  <li className="text-white hover:text-blue-500 hover:border-blue-500 hover:border-b-2 uppercase font-semibold text-xs">
+                    {title}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <hr />
+        {/* ******************************* navlist for small device ******************************* */}
+        <div className="sm: flex justify-between items-center md:hidden py-3 px-2">
+          {/* ------------------ icon section ------------------ */}
+          <div>
+            <div className="relative flex items-center">
+              <h1 className="relative text-2xl font-extrabold uppercase text-white">
+                SkyScape
+              </h1>
+              <img src={mainIcon} alt="" className="w-[60px]" />
+              <p className="absolute top-9 left-14 text-[10px] font-semibold text-white">
+                explore the world
+              </p>
+            </div>
+          </div>
+          {/* ------------------ menu & ul li  section ------------------ */}
+          <div>
+            <img
+              onClick={toogleNavbar}
+              src={menuIcon}
+              alt=""
+              className="w-10"
+            />
+          </div>
+        </div>
+        <hr />
+        {navOpen ? (
+          <ul className="block text-center gap-10 bg-gray-700 py-4">
+            {userNavUl.map(({ path, title }, index) => (
+              <Link key={index} to={path}>
+                <li className="text-white hover:text-blue-500 hover:border-blue-500 hover:border-b-2 uppercase font-semibold leading-10 ">
+                  {title}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
