a2feae54e85107c6141fb4b4a27171a045fd66e8
tamzid620 <tamzid620@gmail.com>
2025-04-10 14:31:01 +0600

    tourCountries compo created also italy section content added

--- src/Components/Shared/UserShared/TourCountries/TourCountries.jsx
+++ src/Components/Shared/UserShared/TourCountries/TourCountries.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { IoLocationOutline } from "react-icons/io5";
+import { IoIosArrowRoundForward } from "react-icons/io";
 import italyMap from "../../../../assets/Images/Map-Italy.jpg";
 import franceMap from "../../../../assets/Images/Map-France.jpg";
 import france1 from "../../../../assets/Images/france-tour/France.jpg";
@@ -11,6 +12,12 @@ import france3 from "../../../../assets/Images/france-tour/Cannes.jpg";
 import france4 from "../../../../assets/Images/france-tour/Paris.jpg";
 import france5 from "../../../../assets/Images/france-tour/Marseille.jpg";
 import france6 from "../../../../assets/Images/france-tour/Bordeaux.jpg";
+import italy1 from "../../../../assets/Images/italy-tour/Roma.jpg";
+import italy2 from "../../../../assets/Images/italy-tour/Italy.jpg";
+import italy3 from "../../../../assets/Images/italy-tour/Venice.jpg";
+import italy4 from "../../../../assets/Images/italy-tour/Milano.jpg";
+import italy5 from "../../../../assets/Images/italy-tour/Perugia.jpg";
+import italy6 from "../../../../assets/Images/italy-tour/Parma.jpg";
 
 const franceData = [
   {
@@ -42,6 +49,38 @@ const franceData = [
     id: 6,
     area: "Bordeaux",
     image: france6
+  },
+];
+const italyData = [
+  {
+    id: 1,
+    area: "Roma",
+    image: italy1
+  },
+  {
+    id: 2,
+    area: "Italy",
+    image: italy2
+  },
+  {
+    id: 3,
+    area: "Venice",
+    image: italy3
+  },
+  {
+    id: 4,
+    area: "Milano",
+    image: italy4
+  },
+  {
+    id: 5,
+    area: "Perugia",
+    image: italy5
+  },
+  {
+    id: 6,
+    area: "Parma",
+    image: italy6
   },
 ];
 
@@ -100,11 +139,11 @@ const TourCountries = () => {
           Travel <span className="text-[#3674B5]">Countries</span>
         </h2>
         {/* <<<<<<<<<<<<<<<<<<<< INFORMATION SECTION  >>>>>>>>>>>>>>>>>>>>>>>> */}
-        <div className="mt-10">
-          <section className="grid lg:grid-cols-2 md:grid-cols-1 sm: grid-cols-1 gap-10 items-center">
+        <div >
+          <section className="grid lg:grid-cols-2 md:grid-cols-1 sm: grid-cols-1 gap-10 items-center mt-10">
             {/* text section   */}
             <div
-              className="bg-cover bg-center p-8 w-full h-full flex items-center"
+              className="bg-contain bg-no-repeat bg-center p-8 w-full h-full flex items-center"
               style={{
                 backgroundImage: `url(${franceMap})`,
                 fontFamily: "Poppins, sans-serif",
@@ -132,6 +171,9 @@ const TourCountries = () => {
                   </div>
                 ))}
               </div>
+              <div className="flex justify-end mt-20">
+              <button className="flex items-center gap-1 bg-[#3674B5] hover:bg-[#578FCA] text-white rounded-sm shadow-black shadow-md px-3 py-2">All Tours <IoIosArrowRoundForward size={20}/></button>
+              </div>
             </div>
             </div>
             {/* slider section  */}
@@ -140,17 +182,17 @@ const TourCountries = () => {
             {franceData.map(({ area, image }, index) => (
               <div
                 key={index}
-                className=" relative overflow-hidden shadow-lg group "
+                className=" relative overflow-hidden group "
               >
                 <img
                   src={image}
                   alt={location}
-                  className="w-full h-[500px] object-cover transition-transform duration-500 group-hover:scale-105 bg-black mx-4"
+                  className=" w-full h-[500px] object-cover transition-transform duration-500 group-hover:scale-105 bg-black mx-4"
                 />
                 <div className="bg-black opacity-0 group-hover:opacity-20 absolute inset-0 z-0 " />
                
-                <div className="absolute bottom-5 left-5 text-white font-semibold  drop-shadow-md opacity-0 group-hover:opacity-100">
-                  <p className="text-2xl flex items-center gap-1 ">
+                <div className="absolute bottom-5 left-5 text-white font-semibold  drop-shadow-md ">
+                  <p className="text-2xl flex items-center gap-1 pb-2">
                     <IoLocationOutline />
                     {area}
                   </p>
@@ -160,6 +202,69 @@ const TourCountries = () => {
             ))}
           </Slider>
             </div>
+          </section>
+          <section className="grid lg:grid-cols-2 md:grid-cols-1 sm: grid-cols-1 gap-10 items-center mt-32">
+            {/* slider section  */}
+            <div>
+            <Slider ref={sliderRef} {...settings}>
+            {italyData.map(({ area, image }, index) => (
+              <div
+                key={index}
+                className=" relative overflow-hidden group "
+              >
+                <img
+                  src={image}
+                  alt={location}
+                  className=" w-full h-[500px] object-cover transition-transform duration-500 group-hover:scale-105 bg-black mx-4"
+                />
+                <div className="bg-black opacity-0 group-hover:opacity-20 absolute inset-0 z-0 " />
+               
+                <div className="absolute bottom-5 left-5 text-white font-semibold  drop-shadow-md ">
+                  <p className="text-2xl flex items-center gap-1 pb-2">
+                    <IoLocationOutline />
+                    {area}
+                  </p>
+                  <hr />
+                </div>
+              </div>
+            ))}
+          </Slider>
+            </div>
+            {/* text section   */}
+            <div
+              className="bg-contain bg-no-repeat bg-center p-8 w-full h-full flex items-center"
+              style={{
+                backgroundImage: `url(${italyMap})`,
+                fontFamily: "Poppins, sans-serif",
+              }}
+            >
+            <div>
+              <h1
+                style={{ fontFamily: "Poppins, sans-serif " }}
+                className="text-3xl uppercase font-bold"
+              >
+                Italy, Europe
+              </h1>
+              <p className="my-4 text-gray-500">
+                We provide you with interesting and exciting tours to different
+                parts of the world. Orci varius natoque penatibus et magnis
+                disney turien nascete ridiculus duru in the mus nellen.
+              </p>
+              <div className="grid grid-cols-3 gap-4 text-gray-500">
+                {italyData.map((italy) => (
+                  <div key={italy.id}>
+                    <p className="flex items-center gap-1 ">
+                      <IoLocationOutline  color="#3674B5"/>
+                      {italy.area}
+                    </p>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-end mt-20">
+              <button className="flex items-center gap-1 bg-[#3674B5] hover:bg-[#578FCA] text-white rounded-sm shadow-black shadow-md px-3 py-2">All Tours <IoIosArrowRoundForward size={20}/></button>
+              </div>
+            </div>
+            </div>   
           </section>
         </div>
       </div>
