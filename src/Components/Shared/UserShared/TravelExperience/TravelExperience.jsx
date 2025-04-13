296be4942e95dd5fee470ef6a8fc5603c7d680fb
tamzid620 <tamzid620@gmail.com>
2025-04-13 13:12:51 +0600

    travel Experience compo added with content

--- src/Components/Shared/UserShared/TravelExperience/TravelExperience.jsx
+++ src/Components/Shared/UserShared/TravelExperience/TravelExperience.jsx
@@ -1,13 +1,11 @@
-import { useRef } from "react";
-import "./TravelExperience.css"
+import { useEffect, useRef } from "react";
+import "./TravelExperience.css";
 import sliderImage1 from "../../../../assets/Images/experience-image/1.jpg";
 import sliderImage2 from "../../../../assets/Images/experience-image/2.jpg";
 import sliderImage3 from "../../../../assets/Images/experience-image/3.jpg";
 import sliderImage4 from "../../../../assets/Images/experience-image/4.jpg";
 import sliderImage5 from "../../../../assets/Images/experience-image/5.jpg";
 import sliderImage6 from "../../../../assets/Images/experience-image/6.jpg";
-import { IoLocationOutline } from "react-icons/io5";
-import { IoIosArrowRoundForward } from "react-icons/io";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -58,95 +56,97 @@ const sliderData = [
 ];
 
 const TravelExperience = () => {
+  const sliderRef = useRef(null);
 
-    const sliderRef = useRef(null);
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        sliderRef.current.slickNext();
+      }, 2000);
 
-    //   useEffect(() => {
-    //     const intervalId = setInterval(() => {
-    //       sliderRef.current.slickNext();
-    //     }, 2000);
-    
-    //     return () => clearInterval(intervalId);
-    //   }, []);
-    
-      const settings = {
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-      };
+      return () => clearInterval(intervalId);
+    }, []);
 
-    return (
-        <div
-        style={{ fontFamily: "Barlow, sans-serif " }}
-        className="bg-blue-950 text-black py-32"
-      >
-        <div className="lg:max-w-6xl md:max-w-3xl sm: max-w-sm lg:mx-auto md:mx-8 sm: mx-5">
-          {/* = INFORMATION SECTION =-=================================================== */}
-          {/* title section  */}
-          <h1 className="tracking-[0.3em] text-[#A1E3F9] font-semibold uppercase mb-2">
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div
+      style={{ fontFamily: "Barlow, sans-serif " }}
+      className="bg-blue-950 text-black py-32"
+    >
+      <div className="lg:max-w-6xl md:max-w-3xl sm: max-w-sm lg:mx-auto md:mx-8 sm: mx-5">
+        {/* = INFORMATION SECTION =-=================================================== */}
+        {/* title section  */}
+        <h1 className="tracking-[0.3em] text-[#A1E3F9] font-semibold uppercase mb-2">
           TRAVEL BLOG
-          </h1>
-          <h2
-            style={{ fontFamily: "Poppins, sans-serif " }}
-            className="text-5xl uppercase font-bold"
-          >
-             <span className="text-white">Travel</span>
+        </h1>
+        <h2
+          style={{ fontFamily: "Poppins, sans-serif " }}
+          className="text-5xl uppercase font-bold"
+        >
+          <span className="text-white">Travel</span>
           <span className="text-[#A1E3F9]"> Experience</span>
-          </h2>
-          {/* <<<<<<<<<<<<<<<<<<<< slider section >>>>>>>>>>>>>>>>>>>>>>>> */}
-          <div className="mt-8">
+        </h2>
+        {/* <<<<<<<<<<<<<<<<<<<< slider section >>>>>>>>>>>>>>>>>>>>>>>> */}
+        <div className="mt-8">
           <Slider ref={sliderRef} {...settings}>
             {sliderData.map(({ image, genre, title, tours }, index) => (
-              <div
-                key={index}
-                className=" relative overflow-hidden group"
-              >
-                <img
-                  src={image}
-                  alt="tour experience images"
-                  className="w-full h-[500px] object-cover transition-transform duration-500 group-hover:scale-105 bg-black mx-4"
-                />
-                <div className="bg-black opacity-0 group-hover:opacity-20 absolute inset-0 z-0 " />
-                <div className="absolute top-3 left-6 bg-[#578FCA] text-white px-3 py-1 rounded-md font-semibold text-sm">
-                  {tours}
-                </div>
-                <div className="absolute bottom-12 left-5 text-white font-semibold  drop-shadow-md opacity-0 group-hover:opacity-100">
-                 
+              <div key={index} className=" relative overflow-hidden group">
+                  <img
+                    src={image}
+                    alt="tour experience images"
+                    className="w-full h-[500px] object-cover transition-transform duration-500 group-hover:scale-105 bg-black mx-4"
+                  />
+                  <div className="bg-black opacity-0 group-hover:opacity-40 absolute inset-0 z-0 " />
+                  <div className="absolute top-3 left-6 bg-[#578FCA] text-white px-3 py-1 rounded-md font-semibold text-sm">
+                    {tours}
                   </div>
+                <div className="absolute bottom-5 left-10 bg-white drop-shadow-md w-[320px] h-[150px] p-6 opacity-0 group-hover:opacity-100">
+                  <h1
+                    style={{ fontFamily: "Poppins, sans-serif " }}
+                    className="text-xl font-light text-[#578FCA]"
+                  >
+                    {genre}
+                  </h1>
+                  <p className="mt-2 font-semibold text-xl">{title}</p>
+                </div>
               </div>
             ))}
           </Slider>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TravelExperience;
